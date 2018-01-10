02e752fb5dda6673692942376bd9d5c21148901f
jynti <33417806+jynti@users.noreply.github.com>
2018-01-10 11:30:35 +0530

    Update sandwich.sql

--- sandwich.sql
+++ sandwich.sql
@@ -1,13 +1,13 @@
-mysql> CREATE TABLE Tastes
+mysql> CREATE TABLE tastes
     -> (
-    -> Name VARCHAR(20) NOT NULL,
-    -> Filling VARCHAR(20) NOT NULL,
-    -> PRIMARY KEY(Name, Filling)
+    -> name VARCHAR(20) NOT NULL,
+    -> filling VARCHAR(20) NOT NULL,
+    -> PRIMARY KEY(name, filling)
     -> )
     -> ENGINE=INNODB;
-Query OK, 0 rows affected (0.70 sec)
+Query OK, 0 rows affected (0.55 sec)
 
-mysql> INSERT INTO Tastes
+mysql> INSERT INTO tastes
     -> VALUES ('Brown', 'Turkey'),
     -> ('Brown', 'Beef'),
     -> ('Brown', 'Ham'),
@@ -15,67 +15,69 @@ mysql> INSERT INTO Tastes
     -> ('Green', 'Beef'),
     -> ('Green', 'Turkey'),
     -> ('Green', 'Cheese');
-
+Query OK, 7 rows affected (0.72 sec)
+Records: 7  Duplicates: 0  Warnings: 0
 
 ///////////////////////////////////////////////////////////////
-mysql> CREATE TABLE Locations
+mysql> CREATE TABLE locations
     -> (
-    -> LName VARCHAR(20) NOT NULL PRIMARY KEY,
-    -> Phone VARCHAR(20),
-    -> Address VARCHAR(40)
+    -> lname VARCHAR(20) NOT NULL PRIMARY KEY,
+    -> phone VARCHAR(20),
+    -> address VARCHAR(40)
     -> )
     -> ENGINE=INNODB;
-Query OK, 0 rows affected (0.73 sec)
+Query OK, 0 rows affected (0.59 sec)
 
-mysql> INSERT INTO Locations
+mysql> INSERT INTO locations
     -> VALUES ('Lincoln', '683 4523', 'Lincoln Place'),
-    -> ('O\'Neill\'s', '674 2134', 'Pearse St'),
+    -> ("O'Neill's", '674 2134', 'Pearse St'),
     -> ('Old Nag', '767 8132', 'Dame St'),
     -> ('Buttery', '702 3421', 'College St');
-Query OK, 4 rows affected (0.08 sec)
+Query OK, 4 rows affected (0.12 sec)
 Records: 4  Duplicates: 0  Warnings: 0
 
+
 /////////////////////////////////////////////////////////////////
-mysql> CREATE TABLE Sandwiches
+mysql> CREATE TABLE sandwiches
     -> (
-    -> Location VARCHAR(20) NOT NULL REFERENCES Locations (LName),
-    -> Bread VARCHAR(20) NOT NULL,
-    -> Filling VARCHAR(20) NOT NULL,
-    -> Price FLOAT,
-    -> PRIMARY KEY (Location, Bread, Filling)
+    -> location VARCHAR(20) NOT NULL,
+    -> bread VARCHAR(20) NOT NULL,
+    -> filling VARCHAR(20) NOT NULL,
+    -> price FLOAT,
+    -> PRIMARY KEY (location, bread, filling),
+    -> FOREIGN KEY (location) REFERENCES locations (lname)
     -> )
     -> ENGINE=INNODB;
-Query OK, 0 rows affected (1.16 sec)
+Query OK, 0 rows affected (0.42 sec)
 
-mysql> INSERT INTO Sandwiches
+mysql> INSERT INTO sandwiches
     -> VALUES ('Lincoln', 'Rye', 'Ham', 1.25),
-    -> ('O\'Neill\'s', 'White', 'Cheese', 1.20),
-    -> ('O\'Neill\'s', 'Whole', 'Ham', 1.25),
+    -> ("O'Neill's", 'White', 'Cheese', 1.20),
+    -> ("O'Neill's", 'Whole', 'Ham', 1.25),
     -> ('Old Nag', 'Rye', 'Beef', 1.35),
     -> ('Buttery', 'White', 'Cheese', 1.00),
-    -> ('O\'Neill\'s', 'White', 'Turkey', 1.35),
+    -> ("O'Neill's", 'White', 'Turkey', 1.35),
     -> ('Buttery', 'White', 'Ham', 1.10),
     -> ('Lincoln', 'Rye', 'Beef', 1.35),
     -> ('Lincoln', 'White', 'Ham', 1.30),
     -> ('Old Nag', 'Rye', 'Ham', 1.40);
-Query OK, 10 rows affected (0.08 sec)
+Query OK, 10 rows affected (0.07 sec)
 Records: 10  Duplicates: 0  Warnings: 0
-
 
 /////////////////////////////////////////////////////////////////
 
 1)
-mysql> SELECT Location
-    -> FROM Sandwiches
-    -> WHERE Filling in
+mysql> SELECT location
+    -> FROM sandwiches
+    -> WHERE filling in
     -> (
-    -> SELECT Filling
-    -> FROM Tastes
-    -> WHERE Name='Jones'
+    -> SELECT filling
+    -> FROM tastes
+    -> WHERE name='Jones'
     -> )
     -> ;
 +-----------+
-| Location  |
+| location  |
 +-----------+
 | Buttery   |
 | O'Neill's |
@@ -85,11 +87,11 @@ mysql> SELECT Location
 ////////////////////////////////////////////////////////////////////
 
 2)
-mysql> SELECT Location
-    -> FROM Tastes AS t NATURAL JOIN Sandwiches AS s
-    -> WHERE t.Name='Jones';
+mysql> SELECT location
+    -> FROM tastes NATURAL JOIN sandwiches
+    -> WHERE name='Jones';
 +-----------+
-| Location  |
+| location  |
 +-----------+
 | Buttery   |
 | O'Neill's |
@@ -99,12 +101,12 @@ mysql> SELECT Location
 ////////////////////////////////////////////////////////////////////
 
 3)
-mysql> SELECT Location, count(distinct Name) AS NumberOfPeople
-    -> FROM Sandwiches NATURAL JOIN Tastes
-    -> GROUP BY Location;
+mysql> SELECT location, count(distinct name) AS number_of_people
+    -> FROM sandwiches NATURAL JOIN tastes
+    -> GROUP BY location;
 
 +-----------+----------------+
-| Location  | NumberOfPeople |
+| location  | number_of_people |
 +-----------+----------------+
 | Buttery   |              3 |
 | Lincoln   |              2 |
